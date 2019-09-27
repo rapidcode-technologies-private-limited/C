44740ca8c2000fe0027c52cfd62aa9cbbd627349
vaishnavisolanki07 <solanki07vaishnavi@gmail.com>
2019-09-27 17:09:37 +0530

    deleting the Vaishnavi directory

--- Vaishnavi/c++/1.6.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	int p=10;
-	int &r=p;
-	cout << "p="<<p<<endl<<"r="<<r<<endl;
-	return 0;
-}
--- Vaishnavi/c++/1.8.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	int s=20;
-	int &a=s;
-	cout<<"s="<<s<<endl<<"a="<<a<<endl;
-	return 0;
-}
